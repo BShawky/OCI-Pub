6290810c25ad40e26e72f1044869009fd8cca620
BShawky <33708556+BShawky@users.noreply.github.com>
2023-05-09 20:20:01 +0300

    variable.tf

--- variable.tf
+++ variable.tf
@@ -1,13 +1,16 @@
+# Tenancy OCID
 variable "tenancy_ocid" {
   default        = "ocid1.tenancy.oc1..aaaaaaaabta7mjh77gvlkf2chnl2w4tgzfeagppzu5utsoisgsjq6z6kcdhq"
 }
 
+# Region
 variable "region" {
     description  = "Techvisons Region"
     type         = string
     default      = "me-jeddah-1"
 }
 
+# Compartmet OCID
 variable "compartment_id" {
     description  = "My Testing Compartment "
     type         = string
