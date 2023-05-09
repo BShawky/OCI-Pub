611e234ea6ca710b075f87a75ec3223595b109d7
BShawky <33708556+BShawky@users.noreply.github.com>
2023-05-09 20:44:55 +0300

    Int-gateway.tf

--- Int-gateway.tf
+++ Int-gateway.tf
@@ -1,6 +1,6 @@
 # This block to - Deploy Internet Gateway
 resource "oci_core_internet_gateway" "IG" {
-  compartment_id   = var.compartment_ocid
+  compartment_id   = var.compartment_id
   display_name     = "Internet Gateway"
   vcn_id           =  oci_core_vcn.tf_vcn.id
 }
