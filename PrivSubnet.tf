079a3dd4dca6bb3115b4d41bbf75ecb6b070019d
BShawky <33708556+BShawky@users.noreply.github.com>
2023-05-09 20:07:10 +0300

    PrivSubnet.tf

--- PrivSubnet.tf
+++ PrivSubnet.tf
@@ -4,6 +4,6 @@ resource "oci_core_subnet" "private_subnet" {
     cidr_block                  = "10.0.1.0/24"
     compartment_id              = var.compartment_id
     prohibit_public_ip_on_vnic  = true
-    display_name                = "privSubnet"
+    display_name                = "Private Subnet"
     dns_label                   = "privsubnet"
 }
