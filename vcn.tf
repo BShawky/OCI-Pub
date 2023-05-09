83b3d61485687151c1eb6115c52ae5dd475aae84
BShawky <33708556+BShawky@users.noreply.github.com>
2023-05-09 20:20:36 +0300

    vcn.tf

--- vcn.tf
+++ vcn.tf
@@ -2,6 +2,6 @@
 resource "oci_core_vcn" "tf_vcn" {
   cidr_block            = "10.0.0.0/16"
   compartment_id        = var.compartment_id
-  display_name          = "terraformVCN"
+  display_name          = "Terraform VCN"
   dns_label             = "terraformvcn"
 }
