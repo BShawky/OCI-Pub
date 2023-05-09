c5e108b8bf51fd4e83e50ebd3eb739550eef111b
BShawky <33708556+BShawky@users.noreply.github.com>
2023-05-09 16:35:06 +0300

    main.tf

--- main.tf
+++ main.tf
@@ -25,7 +25,6 @@ resource "oci_core_subnet" "pub_subnet" {
     vcn_id                      =  oci_core_vcn.tf_vcn.id
     cidr_block                  = "10.0.0.0/24"
     compartment_id              = var.compartment_id
-    prohibit_public_ip_on_vnic  = false
     display_name                = "publicSubnet"
     dns_label                   = "publicsubnet"
 }
